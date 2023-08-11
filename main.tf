7fb853f219fd0edb5d7cfbe68d4962bb1a5e9a3d
Arvind Bageshwar <abageshwar@gmail.com>
2023-08-11 14:08:38 -0700

    added mongodb

--- main.tf
+++ main.tf
@@ -3,55 +3,43 @@ resource "azurerm_cosmosdb_account" "dbaccount" {
   location                  = var.cosmosdb_account_location
   resource_group_name       = var.resourcegroup
   offer_type                = "Standard"
-  kind                      = "GlobalDocumentDB"
-  enable_automatic_failover = false
+  kind                      = "MongoDB"
+  enable_automatic_failover = true
   geo_location {
     location          = var.location
     failover_priority = 0
   }
+  geo_location {
+    location          = var.failover_location
+    failover_priority = 1
+  }
+  
   consistency_policy {
     consistency_level       = "BoundedStaleness"
     max_interval_in_seconds = 300
     max_staleness_prefix    = 100000
   }
-  depends_on = [
-    var.resourcegroup
-  ]
 }
-resource "azurerm_cosmosdb_sql_database" "sqldb" {
-  name                = var.cosmosdb_sqldb_name
+resource "azurerm_cosmosdb_mongo_database" "mongodb" {
+  name                = var.mongodb_name
   resource_group_name = var.resourcegroup
   account_name        = azurerm_cosmosdb_account.dbaccount.name
-  throughput          = var.throughput
-}
-resource "azurerm_cosmosdb_sql_container" "main" {
-  name                  = var.sql_container_name
-  resource_group_name   = var.resourcegroup
-  account_name          = azurerm_cosmosdb_account.dbaccount.name
-  database_name         = azurerm_cosmosdb_sql_database.sqldb.name
-  partition_key_path    = "/definition/id"
-  partition_key_version = 1
-  throughput            = var.throughput
-  
-  indexing_policy {
-    indexing_mode = "consistent"
-
-    included_path {
-      path = "/*"
-    }
-
-    included_path {
-      path = "/included/?"
-    }
-
-    excluded_path {
-      path = "/excluded/?"
-    }
-  }
-
-  unique_key {
-    paths = ["/definition/idlong", "/definition/idshort"]
-  }
+  throughput          = 400
 }
 
+resource "azurerm_cosmosdb_mongo_collection" "coll" {
+  name                = "cosmosmongodbcollection"
+  resource_group_name = azurerm_cosmosdb_account.dbaccount.resource_group_name
+  account_name        = azurerm_cosmosdb_account.dbaccount.name
+  database_name       = azurerm_cosmosdb_mongo_database.mongodb.name
 
+  default_ttl_seconds = "777"
+  shard_key           = "uniqueKey"
+  throughput          = 400
+
+  lifecycle {
+    ignore_changes = [index]
+  }
+
+  depends_on = [azurerm_cosmosdb_mongo_database.mongodb]
+}
--- variables.tf
+++ variables.tf
@@ -7,6 +7,9 @@ variable "location" {
   description = "Resource group location"
 }
 
+variable "failover_location" {
+  default = "westus1"
+}
 variable "resourcegroup" {
   type        = string
   default     = "iacteam2-arv"
@@ -39,28 +42,9 @@ variable "cosmosdb_account_location" {
 }
 
 
-variable "cosmosdb_sqldb_name" {
+variable "mongodb_name" {
   type        = string
-  default     = "arv-sqldb-name"
+  default     = "arv-mongodb"
   description = "value"
 }
 
-variable "sql_container_name" {
-  type        = string
-  default     = "arv-sql-container"
-  description = "SQL API container name."
-}
-
-variable "throughput" {
-  type        = number
-  default     = 400
-  description = "Cosmos db database throughput"
-  validation {
-    condition     = var.throughput >= 400 && var.throughput <= 1000000
-    error_message = "Cosmos db manual throughput should be equal to or greater than 400 and less than or equal to 1000000."
-  }
-  validation {
-    condition     = var.throughput % 100 == 0
-    error_message = "Cosmos db throughput should be in increments of 100."
-  }
-}
