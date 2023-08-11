436401da38edd957299c583d4385916de5b47f98
Arvind Bageshwar <abageshwar@gmail.com>
2023-08-11 14:41:05 -0700

    removed collection creation

--- main.tf
+++ main.tf
@@ -26,20 +26,3 @@ resource "azurerm_cosmosdb_mongo_database" "mongodb" {
   account_name        = azurerm_cosmosdb_account.dbaccount.name
   throughput          = 400
 }
-
-resource "azurerm_cosmosdb_mongo_collection" "coll" {
-  name                = "cosmosmongodbcollection"
-  resource_group_name = azurerm_cosmosdb_account.dbaccount.resource_group_name
-  account_name        = azurerm_cosmosdb_account.dbaccount.name
-  database_name       = azurerm_cosmosdb_mongo_database.mongodb.name
-
-  default_ttl_seconds = "777"
-  shard_key           = "uniqueKey"
-  throughput          = 400
-
-  lifecycle {
-    ignore_changes = [index]
-  }
-
-  depends_on = [azurerm_cosmosdb_mongo_database.mongodb]
-}
